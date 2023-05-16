7befbbd0f088049be3b18b3f79bf5ce483952435
Justin R <jmr18c@acu.edu>
2023-05-16 16:04:07 -0500

    Initial setup and cleanup

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <section className="hero" id="home">
+      <div className="hero-banner">
+        <h1>come explore with us</h1>
+        <p>Beach? Mountains? Safari?</p>
+        <p>
+          We have tours of all kinds that allow you to get out and explore the
+          BackRoads.
+        </p>
+        <a href="#tours" className="btn hero-btn">
+          explore tours
+        </a>
+      </div>
+    </section>
+  )
+}
+export default Hero
